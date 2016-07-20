b22fe1f86f3a86e3fbfed5547b580cf0121566f9
Joe Wood & Lloyd Clave <pair+joe9663.llclave@devbootcamp.com>
2016-07-20 18:11:55 -0400

    adds vote functionality

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -3,3 +3,15 @@ get '/posts/:id' do
   @replies = Reply.all.where(post_id: params[:id])
   erb :'posts/show'
 end
+
+post '/posts/:id/vote' do
+  binding.pry
+  redirect '/login' unless logged_in?
+  post = Post.find_by(id: params[:id])
+  post.votes.new(vote_value: params[:vote].to_i, user_id: session[:user_id], votable_type: post.class.name)
+  if post.votes.last.save
+    redirect '/'
+  else
+    alert('error')
+  end
+end
--- db/migrate/20160720102624_create_votes_table.rb
+++ db/migrate/20160720102624_create_votes_table.rb
@@ -1,8 +1,8 @@
 class CreateVotesTable < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.boolean  :vote_value, null: false
-      t.references :user_id, null: false
+      t.integer  :vote_value, null: false
+      t.references :user, null: false
       t.string   :votable_type, null: false
       t.integer  :votable_id, null: false
 
