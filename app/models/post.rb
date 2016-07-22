141582907a2df01cec733013292edc2d479a633a
Alex Bills <email": null>
2016-07-22 10:39:14 -0400

    added dependent destroy associations to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :votes, as: :votable
-  has_many :replies
+  has_many :votes, as: :votable, dependent: :destroy
+  has_many :replies, dependent: :destroy
 
   def points
     total_votes = Vote.where(votable_type: self.class.name, votable_id: self.id)
