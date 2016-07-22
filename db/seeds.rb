3013d602e0d4ff4551142afa1ec23941428b64d3
Lloyd Clave & Nicholas Wang <pair+llclave.nwang9@devbootcamp.com>
2016-07-22 10:30:18 -0400

    improve seed data, add votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-# 10.times do |num|
-#   User.create(username:"test" + num.to_s, email:"test" + num.to_s, password: "test")
-# end
-# 10.times do
-#   Post.create(title: "test1" + rand(10).to_s , content: "test1", user_id: rand(1..10).to_s)
-# end
-# 100.times do
-#   Reply.create(response: "I have many opinions.", post_id: rand(1..10), user_id: rand(1..10))
-# end
-25.times do
+10.times do
   user = User.create(username: Faker::Name.first_name, email:Faker::Internet.email, password: "test")
   2.times do
-    post = Post.create(title: Faker::ChuckNorris.fact, content: Faker::Lorem.paragraph(2), user_id: rand(1..25))
-    15.times do
-      post.replies.create(response: Faker::StarWars.quote, post_id: post.id, user_id: rand(1..25))
+    post = Post.create(title: Faker::ChuckNorris.fact, content: Faker::Lorem.paragraph(2), user_id: rand(1..10))
+
+    rand(1..50).times do
+      post.votes.create(vote_value: [1,-1].sample, user_id: rand(1..10), votable_type: "Post", votable_id: post.id)
+    end
+
+    10.times do
+      reply = post.replies.create(response: Faker::StarWars.quote, post_id: post.id, user_id: rand(1..10))
+
+      rand(1..50).times do
+        reply.votes.create(vote_value: [1,-1].sample, user_id: rand(1..10), votable_type: "Reply", votable_id: reply.id)
+      end
+
     end
   end
 end
